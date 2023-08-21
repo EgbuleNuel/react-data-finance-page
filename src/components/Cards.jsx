4996ab10cc1e4692679d320a7ff487b344f43021
EgbuleNuel <egbulemmanuel@gmail.com>
2023-08-21 09:43:51 +0100

    project coomplete

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import Single from "../assets/single.png";
+import Double from "../assets/double.png";
+import Triple from "../assets/single.png";
 
 const Cards = () => {
   return (
-    <div>Cards</div>
-  )
-}
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="mx-auto max-w-[1240px] grid md:grid-cols-3 gap-8">
+        <div className="w-full flex flex-col my-4 p-4 rounded-lg shadow-xl hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Single}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
+          <p className="text-center text-4xl font-bold">$149</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">500 Gb Storge</p>
+            <p className="py-2 border-b mx-8">1 User Allowed</p>
+            <p className="py-2 border-b mx-8">Send up to 2 Gb</p>
+          </div>
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black">
+            Start Trial
+          </button>
+        </div>
+        <div className=" bg-gray-100 w-full flex flex-col md:my-0 my-8 p-4 rounded-lg shadow-xl hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-transparent"
+            src={Double}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Partnership</h2>
+          <p className="text-center text-4xl font-bold">$199</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">1 Tb Storge</p>
+            <p className="py-2 border-b mx-8">3 Users Allowed</p>
+            <p className="py-2 border-b mx-8">Send up to 10 Gb</p>
+          </div>
+          <button className="bg-black w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-[#00df9a]">
+            Start Trial
+          </button>
+        </div>
+        <div className="w-full flex flex-col my-4 p-4 rounded-lg shadow-xl hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Triple}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Group Account</h2>
+          <p className="text-center text-4xl font-bold">$299</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">5 Tb Storge</p>
+            <p className="py-2 border-b mx-8">10 Users Allowed</p>
+            <p className="py-2 border-b mx-8">Send up to 20 Gb</p>
+          </div>
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black">
+            Start Trial
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
